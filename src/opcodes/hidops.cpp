8091b10d7fced05200e36ffdb3a896ae2c0effbd
veplaini <victor.lazzarini@nuim.ie>
2018-06-15 12:12:12 +0100

    sony opcode

--- src/opcodes/hidops.cpp
+++ src/opcodes/hidops.cpp
@@ -10,7 +10,6 @@
 
 // HDI device list printing
 struct HIDPrint : csnd::Plugin<0, 0> {
-
   int init() {
     int i = 0;
     struct hid_device_info *devs, *cur_dev;
@@ -40,9 +39,60 @@ struct HIDPrint : csnd::Plugin<0, 0> {
   }
 };
 
+struct SonyJoy : csnd::Plugin<1, 2> {
+   hid_device *handle;
+
+   int deinit() {
+     hid_close(handle)
+       return OK;
+   }
+
+   int init() {
+     int i = 0;
+     struct hid_device_info *devs, *cur_dev;
+     devs = hid_enumerate(0, 0);
+     cur_dev = devs;
+      while (cur_dev) {
+    // USAGE = 1  (general desktop)
+    // USAGE PAGE = 5 (game controls)
+    // VENDOR ID = 0x54C
+    // PID = 0x5C4 
+       if (cur_dev->usage_page == 1 && cur_dev->usage == 5 &&
+        cur_dev->vendor_id == 0x54C && cur_dev->product_id == 0x5C4) {
+      break;
+    }
+    cur_dev = cur_dev->next;
+    i++;
+    }
+    if (cur_dev) {
+      handle = hid_open_path(cur_dev->path);
+      if (!handle) {
+        csound->message("unable to open device\n");
+        return NOTOK;
+    } 
+   } else return NOTOK;
+    hid_set_nonblocking(handle, 1);
+    csound->plugin_deinit(this);
+    hid_free_enumeration(devs);
+    return OK;
+   }
+
+  int kperf() {
+     int res;
+     unsigned char buf[128], mask;
+     res = hid_read(handle, buf, sizeof(buf));
+     mask = (unsigned char) (inargs[1] > 0 ? inargs[1] : 0xFF);
+     if(res)
+       outargs[0] = buf[(int) inargs[0]] & mask;
+     return OK;
+  } 
+};
+
+  
 #include <modload.h>
 void csnd::on_load(Csound *csound) {
   csnd::plugin<HIDPrint>(csound, "hidprint", "", "", csnd::thread::i);
+  csnd::plugin<HIDPrint>(csound, "sonyctl", "k", "kk", csnd::thread::ik);
   // tracing for debugging purposes, remove when ready.
   csound->message("HID opcode library loaded \n");
 }
